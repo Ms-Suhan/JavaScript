2bdc3a999a36b2aefd37228757f75aaa8bd54cb6
Ms-Suhan <mssuhan7@gmail.com>
2025-06-23 18:58:27 +0530

    new commit

--- Chapter_04/04_foreach.js
+++ Chapter_04/04_foreach.js
@@ -1,4 +1,28 @@
 const coding = ["js", "ruby", "java", "cpp", "python"]
 
 
-coding.forEach((item)=>(console.log(item)))
+// coding.forEach((item)=>(console.log(item)))
+
+const mycoding = [
+    {
+        languageName: "javascript",
+        languageFileName: 'js'
+    },
+    {
+        languageName: "Python",
+        languageFileName: 'py'
+    },
+    {
+        languageName: "java",
+        languageFileName: 'java'
+    },
+    {
+        languageName: "typescript",
+        languageFileName: 'ts'
+    }
+]
+
+
+mycoding.forEach((item) => {
+    console.log(item.languageFileName)
+})
